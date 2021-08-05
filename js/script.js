6afd3a6a3b98523e78e3df946c3abf8737a81872
Emily <emilyalmeeidas@gmail.com>
2021-08-05 16:32:44 -0300

    inserindo botão

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const buttonAdd = document.getElementById('btn')
+const input = document.getElementById('myInput')
+
+buttonAdd.addEventListener('click', addActivity)
+
+function addActivity() {
+    //console.log('Entrou')
+}
+
